2bda24565db61f15c3cbcebea6f7850a03a535d6
mauricechouam <lansichouamou@gmail.com>
2025-02-27 19:26:38 -0800

    Update about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,12 @@ const About = () => {
   return (
     <section id="about">
       <h2>About Me</h2>
-      <p>As an independent application developer, I have worked extensively on complex systems, taking projects from initial concept to full deployment.</p>
+      <p>I am a Full Stack Developer with a strong problem-solving focus and a passion for building efficient, user-friendly 
+        applications. With over six years of experience in system administration and IT infrastructure, I bring a detail-oriented 
+        mindset to software development. I have expertise in JavaScript, HTML, CSS, and frameworks like React, Node.js, and Express.
+         My experience includes designing and implementing web applications at UC Davis Health, where I streamline workflows 
+         and enhance user experience. I also hold AWS certifications,
+         demonstrating my proficiency in cloud technologies.</p>
     </section>
   );
 };
